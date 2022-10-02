9e6bd5643219a4249f6024663be4565ec89246a5
always_wsl <wnwngudwns@gmail.com>
2022-10-02 14:46:38 +0900

    ex2 done

--- ex2/dir_search.js
+++ ex2/dir_search.js
@@ -1,2 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+const s_pos = "./test";
+
+get_js(s_pos);
+
+function get_js(location) {
+
+	const fs = require('fs');
+	const path = require('path');
+	const util = require('util');
+
+	fs.stat(location, function(err, stat) {
+		if(stat.isDirectory() == true)
+		{
+
+		const read_dir = util.promisify(fs.readdir);
+
+			read_dir(location, function(err, fileList){
+				if(!err){
+					fileList.forEach(async file => {
+						file = path.join(location, file);
+						
+						await get_js(file);
+					});
+				}
+				else console.err(err);
+			});
+
+		}
+		else if(path.extname(location) == '.js')
+			console.log(location);
+	});
+}
